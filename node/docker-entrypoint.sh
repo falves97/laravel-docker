4cfe8fe66ebfa1b44602d9773f0ed5fc4c68ff87
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-06-26 04:16:14 -0300

    refac: sempre instalar pacotes e alterado local dos certs

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ ! -f "node/localhost.key" ]; then
-  cd node
+if [ ! -f "node/certs/localhost.key" ]; then
+  cd node/certs
   openssl req -newkey rsa:4096 \
               -x509 \
               -sha256 \
@@ -11,19 +11,13 @@ if [ ! -f "node/localhost.key" ]; then
               -out localhost.crt \
               -keyout localhost.key \
               -subj "/C=BR/ST=Estado/L=Cidade/O=Company Name/OU=Unity Compane Name/CN=localhost"
-  cd ..
+  cd ../..
 fi
 
 if [ "$1" = 'yarn' ]; then
-
   if [ -z "$(ls -A 'node_modules/' 2>/dev/null)" ] && [ -f "package.json" ]; then
-    if [ $NODE_ENV = "development" ]; then
       yarn --non-interactive
-    else
-      yarn --production --frozen-lockfile --non-interactive
-    fi
   fi
-
 fi
 
 exec "$@"
